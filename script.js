97151accd7c7494ba99edd51906b11b0da465321
sachinmaindan <sachinmaindan12@gmail.com>
2023-12-31 13:24:22 +0530

    button onclick funtions addded

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayAlertmsg()
+{
+    window.alert("Welcome to Motoworld Arena")
+}
+
+
+function openArticle() {
+    window.open('https://www.zigwheels.com/reviews-advice/shootouts/indias-best-160cc-sporty-commuter-is/50052/', '_blank');
+  }
